320e43679e2d73702ba253b7f0916e7ce2deadaa
vikramsingh301093 <vikram.singhkushwaha@atos.net>
2018-11-01 17:56:23 +0530

    modified excel files

--- template/Dynamic_Oracle_Add_Users.sh
+++ template/Dynamic_Oracle_Add_Users.sh
@@ -2,7 +2,7 @@
 #mkdir /home/ec2-user/Oracle_User_Management
 cd /u01/app/oracle/product/11.2.0/xe/bin
 . ./oracle_env.sh
-input="/home/ec2-user/Oracle_User.csv"
+input="/home/ec2-user/Oracle_Add_User.csv"
 while IFS=',' read -r f1 f2 f3 f4 f5
 do
 echo "sqlplus system/atos123 <<EOF
